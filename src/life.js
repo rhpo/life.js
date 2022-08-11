3cadb7b7064633536c9981b440937e949ca7aaae
Ramy <69460661+rhpo@users.noreply.github.com>
2022-08-11 12:58:32 +0100

    Added the GameLoop Class ♾️

--- src/life.js
+++ src/life.js
@@ -3,6 +3,56 @@ export const id = () => Math.random().toString(36).substring(2, 9), getRandomNam
     let names = namelist.split(';');
     return names[Math.floor(Math.random() * names.length)];
 }, defined = e => e !== undefined && e !== null;
+
+export class GameLoop {
+    constructor(world, fps = 60) {
+        var delay = 1000 / fps,
+            time = null,
+            frame = -1,
+            tref;
+        var callback = world;
+
+        function loop(timestamp) {
+            if (time === null) time = timestamp;
+            var seg = Math.floor((timestamp - time) / delay);
+            if (seg > frame) {
+                frame = seg;
+                callback({
+                    time: timestamp,
+                    frame: frame
+                })
+            }
+            tref = requestAnimationFrame(loop)
+        }
+
+        this.isPlaying = false;
+
+        this.frameRate = function (newfps) {
+            if (!arguments.length) return fps;
+            fps = newfps;
+            delay = 1000 / fps;
+            frame = -1;
+            time = null;
+        };
+
+        this.start = function () {
+            if (!this.isPlaying) {
+                this.isPlaying = true;
+                tref = requestAnimationFrame(loop);
+            }
+        };
+
+        this.pause = function () {
+            if (this.isPlaying) {
+                cancelAnimationFrame(tref);
+                this.isPlaying = false;
+                time = null;
+                frame = -1;
+            }
+        };
+    }
+}
+
 let workingWorld = {};
 
 const eventify = self => {
