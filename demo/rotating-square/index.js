b6254020a58f88f175556d76bfaf4e0b1245d51e
Ramy <69460661+rhpo@users.noreply.github.com>
2022-09-30 21:07:34 +0100

    Changed "setInterval" to "GameLoop" class.

--- demo/rotating-square/index.js
+++ demo/rotating-square/index.js
@@ -1,7 +1,9 @@
-import { World, Shape } from 'https://min.gitcdn.link/cdn/rhpo/life.js/main/src/life.min.js'
+import { World, Shape, GameLoop } from 'https://min.gitcdn.link/cdn/rhpo/life.js/main/src/life.min.js'
 
 // Rotating Centered Square...
 const world = new World();
+
+// Make the Square shape instance...
 const square = new Shape({
     type: 'rectangle',
     width: 80,
@@ -19,4 +21,6 @@ function Main() {
 
     world.center(square) // • Make the square centered in the Screen.
 }
-setInterval(Main, 1000 / FPS);
+
+const game = new GameLoop(Main, FPS);
+game.start();
